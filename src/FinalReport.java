59f8f0c00d4adae0f9654c9a1ccf22f7ea27d8e1
Leonardo Lima <123819366+Lioneriod@users.noreply.github.com>
2024-11-27 19:52:18 -0300

    waiter, please more inheritances!

---
+++ src/FinalReport.java
@@ -0,0 +1,7 @@
+public class FinalReport extends ReportGenerator{
+
+    @Override
+    public void generateReport() {
+        
+    }
+}
---
+++ src/Yappers.java
@@ -0,0 +1,8 @@
+//This one is to print all that big wall of text I made
+public class Yappers extends ReportGenerator {
+
+    @Override
+    public void generateReport() {
+
+    }
+}
